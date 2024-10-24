5984e7287b93ea7f62f48860ea261a58d395ffa3
Yash Desale <55501058+whopee@users.noreply.github.com>
2024-10-24 21:12:25 +0530

    Add files via upload

---
+++ templates/python_section_1.py
@@ -0,0 +1,96 @@
+from typing import Dict, List
+
+import pandas as pd
+
+
+def reverse_by_n_elements(lst: List[int], n: int) -> List[int]:
+    """
+    Reverses the input list by groups of n elements.
+    """
+    # Your code goes here.
+    return lst
+
+
+def group_by_length(lst: List[str]) -> Dict[int, List[str]]:
+    """
+    Groups the strings by their length and returns a dictionary.
+    """
+    # Your code here
+    return dict
+
+def flatten_dict(nested_dict: Dict, sep: str = '.') -> Dict:
+    """
+    Flattens a nested dictionary into a single-level dictionary with dot notation for keys.
+    
+    :param nested_dict: The dictionary object to flatten
+    :param sep: The separator to use between parent and child keys (defaults to '.')
+    :return: A flattened dictionary
+    """
+    # Your code here
+    return dict
+
+def unique_permutations(nums: List[int]) -> List[List[int]]:
+    """
+    Generate all unique permutations of a list that may contain duplicates.
+    
+    :param nums: List of integers (may contain duplicates)
+    :return: List of unique permutations
+    """
+    # Your code here
+    pass
+
+
+def find_all_dates(text: str) -> List[str]:
+    """
+    This function takes a string as input and returns a list of valid dates
+    in 'dd-mm-yyyy', 'mm/dd/yyyy', or 'yyyy.mm.dd' format found in the string.
+    
+    Parameters:
+    text (str): A string containing the dates in various formats.
+
+    Returns:
+    List[str]: A list of valid dates in the formats specified.
+    """
+    pass
+
+def polyline_to_dataframe(polyline_str: str) -> pd.DataFrame:
+    """
+    Converts a polyline string into a DataFrame with latitude, longitude, and distance between consecutive points.
+    
+    Args:
+        polyline_str (str): The encoded polyline string.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing latitude, longitude, and distance in meters.
+    """
+    return pd.Dataframe()
+
+
+def rotate_and_multiply_matrix(matrix: List[List[int]]) -> List[List[int]]:
+    """
+    Rotate the given matrix by 90 degrees clockwise, then multiply each element 
+    by the sum of its original row and column index before rotation.
+    
+    Args:
+    - matrix (List[List[int]]): 2D list representing the matrix to be transformed.
+    
+    Returns:
+    - List[List[int]]: A new 2D list representing the transformed matrix.
+    """
+    # Your code here
+    return []
+
+
+def time_check(df) -> pd.Series:
+    """
+    Use shared dataset-2 to verify the completeness of the data by checking whether the timestamps for each unique (`id`, `id_2`) pair cover a full 24-hour and 7 days period
+
+    Args:
+        df (pandas.DataFrame)
+
+    Returns:
+        pd.Series: return a boolean series
+    """
+    # Write your logic here
+
+    return pd.Series()
